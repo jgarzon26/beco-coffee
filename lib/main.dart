cd19e3fc1fad0323a5959b8a00cb15f88b39baa1
jgarzon26 <janus.garzon@usjr.edu.ph>
2024-09-23 15:34:49 +0800

    sample splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:beco_coffee/home/view/home_screen.dart';
 import 'package:beco_coffee/intro/view/intro_screen.dart';
 import 'package:beco_coffee/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:go_router/go_router.dart';
 
-void main() {
+Future main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(
+    const Duration(milliseconds: 1000),
+    () {
+      FlutterNativeSplash.remove();
+    },
+  );
   runApp(const MyApp());
 }
 
