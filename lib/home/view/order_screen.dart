39f0adc7c015e19557a56508b320a837f90fae9b
jgarzon26 <janus.garzon@usjr.edu.ph>
2025-02-27 12:56:06 +0800

    order screen refactor

--- lib/home/view/order_screen.dart
+++ lib/home/view/order_screen.dart
@@ -1,11 +1,9 @@
-import 'package:beco_coffee/home/model/coffee.dart';
 import 'package:beco_coffee/home/model/order.dart';
 import 'package:beco_coffee/home/repo/coffee_repo.dart';
-import 'package:beco_coffee/theme/theme.dart';
+import 'package:beco_coffee/home/widget/order/order_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
@@ -117,88 +115,6 @@ class _OrderScreenState extends State<OrderScreen> {
             },
           ),
           const Gap(10),
-        ],
-      ),
-    );
-  }
-}
-
-class OrderTile extends StatelessWidget {
-  const OrderTile({
-    super.key,
-    required this.coffee,
-    required this.status,
-  });
-
-  final Coffee coffee;
-  final OrderStatus status;
-
-  @override
-  Widget build(BuildContext context) {
-    final statusRecord = switch (status) {
-      OrderStatus.onDelivery => ('Delivery', Colors.lime),
-      OrderStatus.process => ('Process', const Color.fromRGBO(255, 4, 95, 1)),
-      OrderStatus.success => (
-          'Completed',
-          const Color.fromRGBO(255, 193, 0, 1)
-        ),
-    };
-
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(20)),
-        color: Colors.black.withValues(alpha: 0.05),
-      ),
-      padding: const EdgeInsets.all(15),
-      child: Row(
-        children: [
-          Expanded(
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                child: FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: coffee.image_src,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          const Gap(15),
-          Expanded(
-            flex: 2,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  coffee.name,
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const Gap(5),
-                Text(
-                  '1 items',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                const Gap(5),
-                Text(
-                  '1.5\$',
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const Gap(5),
-                Text(
-                  statusRecord.$1,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: statusRecord.$2,
-                      ),
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
